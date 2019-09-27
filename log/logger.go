37fef2a9138d6424e2d57ea0b9cb6a066c43fd12
vincent <vincent@cdm.com>
2019-09-27 08:57:59 +0800

    优化日志打印, warn 跟info级别的日志分文件存放. development环境打印到 控制台

--- log/logger.go
+++ log/logger.go
@@ -4,6 +4,7 @@ import (
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
 )
 
 var (
@@ -23,30 +24,62 @@ func Initialize(config ...zap.Config) *zap.Logger {
 	} else {
 		cfg = config[0]
 
-		hook := lumberjack.Logger{
+		sink := lumberjack.Logger{
 			Filename:   cfg.OutputPaths[0], // 日志文件路径
-			MaxSize:    1024,     // megabytes
-			MaxBackups: 7,        // 最多保留3个备份
-			MaxAge:     30,       //days
-			Compress:   true,     // 是否压缩 disabled by default
+			MaxSize:    500,                // megabytes
+			MaxBackups: 20,                 // 最多保留3个备份
+			MaxAge:     30,                 //days
+			Compress:   true,               // 是否压缩 disabled by default
 		}
+
+		errSink := lumberjack.Logger{
+			Filename:   cfg.ErrorOutputPaths[0], // 日志文件路径
+			MaxSize:    1024,                    // megabytes
+			MaxBackups: 1,                       // 最多保留3个备份
+			MaxAge:     30,                      //days
+			Compress:   true,                    // 是否压缩 disabled by default
+		}
+
+		cfg.EncoderConfig = zap.NewProductionEncoderConfig()
 
 		cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 		cfg.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
 		cfg.EncoderConfig.LineEnding = zapcore.DefaultLineEnding
 
-		core := zapcore.NewCore(
-			zapcore.NewConsoleEncoder(cfg.EncoderConfig),
-			zapcore.AddSync(&hook),
-			cfg.Level,
-		)
+		// 实现两个判断日志等级的interface
+		infoLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl < zapcore.WarnLevel
+		})
 
-		logger = zap.New(core)
+		warnLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl >= zapcore.WarnLevel
+		})
+
+		// zapcore.NewTee 控制不同级别日志输出到不同源
+		var zapcores []zapcore.Core
+		encoder := zapcore.NewConsoleEncoder(cfg.EncoderConfig)
+		zapcores = append(zapcores, zapcore.NewCore(encoder, zapcore.AddSync(&sink), infoLevel))
+		zapcores = append(zapcores, zapcore.NewCore(encoder, zapcore.AddSync(&errSink), warnLevel))
+
+		// 开发模式, debug级别以上全部打印到控制台
+		if cfg.Development {
+			zapcores = append(zapcores, zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel))
+		}
+		core := zapcore.NewTee(zapcores...)
+
+		// 设置初始化字段 地址后面追加字段
+		//filed := zap.Fields(zap.String("serviceName", "Starfire-OTC"))
+
+		logger = zap.New(core, zap.AddCaller(), zap.Development(), zap.AddCallerSkip(1), zap.ErrorOutput(zapcore.AddSync(&errSink)))
 	}
 
 	sugaredLogger = logger.Sugar()
-	logger.Info("DefaultLogger INIT success")
+	logger.Info(">>>> DefaultLogger INIT success <<<<")
 
+	return logger
+}
+
+func GetLogger() *zap.Logger {
 	return logger
 }
 
